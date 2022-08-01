25688cc6f4b8c1220f430f0fc509b379387b61e4
Kuba <jacob.klin@buddy.works>
2022-08-01 14:47:12 +0200

    work work

--- scripts.js
+++ scripts.js
@@ -53,3 +53,23 @@ $(window).scroll(function () {
     }
   });
 });
+
+$(document).ready(function () {
+  $('.animate').each(function (i) {
+    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    /* If the object is completely visible in the window, fade it it */
+    if (bottom_of_window > bottom_of_object - 50) {
+      $(this).addClass('moveIn');
+    }
+  });
+
+  $('.animate-earlier').each(function (i) {
+    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    /* If the object is completely visible in the window, fade it it */
+    if (bottom_of_window > bottom_of_object - 250) {
+      $(this).addClass('moveIn');
+    }
+  });
+});
